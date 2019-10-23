eb37d07a6be77eaf30d9ae8f8bb50f69f6f04cf4
wangxuefeng <wangxuefeng2@xiaoniu66.com>
2019-10-23 13:50:28 +0800

    测试

--- TestYogaKit/ViewController.m
+++ TestYogaKit/ViewController.m
@@ -23,10 +23,18 @@
     [self tagLayout];
     [self inputLayout];
     [self mixAutoLayout];
+    [self mLayout];
+    [self alignLayout];
+    
+    BOOL res1 = [[NSObject class] isKindOfClass:[NSObject class]];
+    BOOL res2 = [[NSObject class] isMemberOfClass:[NSObject class]];
+    BOOL res3 = [[ViewController class] isKindOfClass:[ViewController class]];
+    BOOL res4 = [[ViewController class] isMemberOfClass:[ViewController class]];
+    NSLog(@"%d-%d-%d-%d",res1, res2, res3, res4);
 }
 
 - (void)tagLayout {
-    NSArray *tags = @[@"投资理财",@"超高收益",@"七日年化收益",@"支付宝",@"微信",@"云闪付",@"花呗"];
+    NSArray *tags = @[@"微信",@"投资理财",@"七日年化收益",@"支付宝",@"超高收益",@"云闪付",@"花呗"];
     
     UIView *tagBgView = [[UIView alloc] initWithFrame:CGRectMake(0, 100, 414, 100)];
     
@@ -116,10 +124,125 @@
     redView.translatesAutoresizingMaskIntoConstraints = NO;
     
     NSArray *hLayout = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[redView]-20-|" options:NSLayoutFormatAlignAllTop metrics:nil views: NSDictionaryOfVariableBindings(redView)];
-    NSArray *yLayout = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-200-[redView]-20-|" options:NSLayoutFormatAlignAllTop metrics:nil views: NSDictionaryOfVariableBindings(redView)];
+    
+    NSNumber *h = @([UIScreen mainScreen].bounds.size.width);
+    NSArray *yLayout = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-240-[redView(180)]|" options:NSLayoutFormatAlignAllTop metrics:@{@"h":h} views: NSDictionaryOfVariableBindings(redView)];
     
     [self.view addConstraints:hLayout];
     [self.view addConstraints:yLayout];
+    
+    
+    NSArray *tags = @[@"投资理财",@"超高收益",@"七日年化收益",@"支付宝",@"微信",@"云闪付",@"花呗"];
+       
+       UIView *tagBgView = [[UIView alloc] initWithFrame:CGRectZero];
+
+       [tagBgView configureLayoutWithBlock:^(YGLayout * _Nonnull layout) {
+           layout.isEnabled = YES;
+           layout.flexDirection = YGFlexDirectionColumn;
+           layout.marginTop = YGPointValue(10);
+           layout.paddingBottom = YGPointValue(10);
+           layout.height = YGPointValue(140); 
+           layout.flexWrap = YGWrapWrap;
+           layout.alignContent = YGAlignCenter;
+           layout.justifyContent = YGAlignCenter;
+       }];
+       
+       [redView addSubview:tagBgView];
+       
+       for (NSString *obj in tags) {
+           UILabel *label = [[UILabel alloc] init];
+
+           label.text = obj;
+           label.backgroundColor = [UIColor orangeColor];
+           [label configureLayoutWithBlock:^(YGLayout * _Nonnull layout) {
+               layout.isEnabled =YES;
+               layout.marginLeft = YGPointValue(10);
+               layout.marginTop = YGPointValue(10);
+           }];
+           
+           [tagBgView addSubview:label];
+       }
+    
+    redView.yoga.isEnabled = YES;
+    [redView.yoga applyLayoutPreservingOrigin:YES];
+}
+
+
+- (void)mLayout {
+    UIView *redView = [[UIView alloc] init];
+    redView.backgroundColor = [UIColor greenColor];    
+    [self.view addSubview:redView];
+
+    [redView configureLayoutWithBlock:^(YGLayout * _Nonnull layout) {
+        layout.isEnabled = YES;
+        layout.height = YGPointValue(140);
+        layout.width = YGPointValue([UIScreen mainScreen].bounds.size.width-40);
+        layout.marginTop = YGPointValue(20);
+        layout.marginLeft = YGPointValue(20);
+        layout.flexDirection = YGFlexDirectionColumn;
+        layout.justifyContent = YGJustifyFlexEnd;
+        
+    
+    }];
+    
+    NSArray *tags = @[@"七日年化收益",@"支付宝",@"超高收益"];
+
+    for (NSString *obj in tags) {
+       UILabel *label = [[UILabel alloc] init];
+
+       label.text = obj;
+       label.backgroundColor = [UIColor orangeColor];
+       [label configureLayoutWithBlock:^(YGLayout * _Nonnull layout) {
+           layout.isEnabled =YES;
+           layout.marginLeft = YGPointValue(10);
+           layout.marginTop = YGPointValue(10);
+       }];
+       
+       [redView addSubview:label];
+    }
+    
+    redView.yoga.isEnabled = YES;
+    [redView.yoga applyLayoutPreservingOrigin:YES];
+    [self.view.yoga applyLayoutPreservingOrigin:NO];
+}
+
+
+- (void)alignLayout {
+    UIView *redView = [[UIView alloc] init];
+      redView.backgroundColor = [UIColor greenColor];    
+      [self.view addSubview:redView];
+
+      [redView configureLayoutWithBlock:^(YGLayout * _Nonnull layout) {
+          layout.isEnabled = YES;
+          layout.height = YGPointValue(140);
+          layout.width = YGPointValue([UIScreen mainScreen].bounds.size.width-40);
+          layout.marginTop = YGPointValue(20);
+          layout.marginLeft = YGPointValue(20);
+          layout.flexDirection = YGFlexDirectionRow;
+          layout.alignItems = YGAlignCenter;
+          layout.justifyContent = YGJustifySpaceBetween;
+      }];
+      
+      NSArray *tags = @[@"微信",@"投资理财",@"七日年化收益",@"支付宝"];
+         
+         for (NSString *obj in tags) {
+             UILabel *label = [[UILabel alloc] init];
+
+             label.text = obj;
+             label.backgroundColor = [UIColor orangeColor];
+             [label configureLayoutWithBlock:^(YGLayout * _Nonnull layout) {
+                 layout.isEnabled =YES;
+                 layout.marginLeft = YGPointValue(10);
+                 layout.height = YGPointValue(15*obj.length);
+             }];
+             
+             [redView addSubview:label];
+         }
+      
+      redView.yoga.isEnabled = YES;
+      [redView.yoga applyLayoutPreservingOrigin:YES];
+      [self.view.yoga applyLayoutPreservingOrigin:NO];
+    
 }
 
 @end
